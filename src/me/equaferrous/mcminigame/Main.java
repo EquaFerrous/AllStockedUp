8237a66dfe7bd20579b5a90d80f849e2569fb70d
benbrooks15 <102412238+EquaFerrous@users.noreply.github.com>
2022-08-05 22:50:50 +0100

    Initial setup

---
+++ src/me/equaferrous/mcminigame/Main.java
@@ -0,0 +1,28 @@
+package me.equaferrous.mcminigame;
+
+import me.equaferrous.mcminigame.utility.MessageSystem;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Plugin plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+        MessageSystem.opBroadcast("Plugin enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        MessageSystem.opBroadcast("Plugin disabled.");
+    }
+
+    // -------------------------------------------
+
+    public static Plugin getPlugin() {
+        return plugin;
+    }
+}
